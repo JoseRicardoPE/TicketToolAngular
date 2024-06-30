4b76bbb87446320a1df077bd4b014c6d16c87b90
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2024-06-30 18:40:46 -0500

    create new components for the ticket tool project and add docs about the project

--- TicketToolAngular/src/app/app.module.ts
+++ TicketToolAngular/src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './components/department/department.component';
+import { ParentCategoryComponent } from './components/parent-category/parent-category.component';
+import { ChildCategoryComponent } from './components/child-category/child-category.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { NewTicketComponent } from './components/new-ticket/new-ticket.component';
+import { TicketListComponent } from './components/ticket-list/ticket-list.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    ParentCategoryComponent,
+    ChildCategoryComponent,
+    EmployeeComponent,
+    NewTicketComponent,
+    TicketListComponent,
+    DashboardComponent,
+    LoginComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
---
+++ TicketToolAngular/src/app/components/parent-category/parent-category.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-category',
+  templateUrl: './parent-category.component.html',
+  styleUrls: ['./parent-category.component.css']
+})
+export class ParentCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
