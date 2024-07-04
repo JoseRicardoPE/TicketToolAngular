a59a4e3b3f5823e54fa337261aebb8746a6aa377
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2024-07-04 11:48:10 -0500

    add layout template and onLogOff() method

--- TicketToolAngular/src/app/components/layout/layout.component.ts
+++ TicketToolAngular/src/app/components/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onLogOff() {
+    localStorage.removeItem('ticketUser');
+    this.router.navigateByUrl('login');
   }
 
 }
