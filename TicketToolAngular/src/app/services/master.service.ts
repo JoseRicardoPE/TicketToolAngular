6b055acac719a65210d3eae86a6fab8a0cf695bc
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2024-07-28 16:54:13 -0500

    fix methods on service

--- TicketToolAngular/src/app/services/master.service.ts
+++ TicketToolAngular/src/app/services/master.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Login } from '../interfaces/login';
-import { NewDeptObj } from '../interfaces/new-dept-obj';
+import { DeptObj } from '../interfaces/new-dept-obj';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -19,20 +19,20 @@ export class MasterService {
     return this.http.post<any>(`${this.apiURL}Login`, obj);
   }
 
-  getDepartments() {
+  getAllDepartments() {
     return this.http.get(`${this.apiURL}GetDepartments`);
   }
 
-  createDepartment(obj: NewDeptObj): Observable<any> {
+  createDepartment(obj: DeptObj): Observable<any> {
     return this.http.post(`${this.apiURL}CreateDepartment`, obj);
   }
 
-  updateDepartment(obj: any) {
-    this.http.put(`${this.apiURL}UpdateDepartment`, obj);
+  updateDepartment(obj: DeptObj): Observable<any> {
+    return this.http.put(`${this.apiURL}UpdateDepartment`, obj);
   }
 
   deleteDepartment(id: number) {
-    this.http.delete(`${this.apiURL}DeleteDepartment?id=${id}`, );
+    return this.http.delete(`${this.apiURL}DeleteDepartment?id=${id}`, );
   }
 
 }
