76966e9cf8b592e3b4170c2da167b1209e7d4d7b
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2024-08-11 17:01:27 -0500

    add routes to department, parentCategory, and childCategory components

--- TicketToolAngular/src/app/app-routing.module.ts
+++ TicketToolAngular/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './components/login/login.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DepartmentComponent } from './components/department/department.component';
+import { ParentCategoryComponent } from './components/parent-category/parent-category.component';
+import { ChildCategoryComponent } from './components/child-category/child-category.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -11,7 +13,9 @@ const routes: Routes = [
   { path: '', component: LayoutComponent,
     children: [
       { path: 'dashboard', component: DashboardComponent },
-      { path: 'department', component: DepartmentComponent }
+      { path: 'department', component: DepartmentComponent },
+      { path: 'parent-category', component: ParentCategoryComponent },
+      { path: 'child-category', component: ChildCategoryComponent },
     ]
   }
 ];
