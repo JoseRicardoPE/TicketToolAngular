f1865cf5e697ec8c89e1524cf885dc9d308bc85d
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2024-08-17 17:51:35 -0500

    Add childCategoryObj interface

--- TicketToolAngular/src/app/interfaces/child-category-obj.ts
+++ TicketToolAngular/src/app/interfaces/child-category-obj.ts
@@ -1,4 +1,9 @@
 export interface ChildCategoryObj {
+    childCategoryId: number;
+    categoryName: string;
+    parentCategoryId: number;
+}
+export interface getChildCategoryObj {
     categoryName: string;
     parentCategoryName: string;
     childCategoryId: number;
