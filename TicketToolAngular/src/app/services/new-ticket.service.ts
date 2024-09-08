8e22b7c25c4ae64773788c4c3ba1357520b8c287
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2024-09-08 18:52:39 -0500

    create new-ticket service

---
+++ TicketToolAngular/src/app/services/new-ticket.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CreateNewTicket } from '../interfaces/create-new-ticket';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewTicketService {
+
+  apiURL: string = 'https://freeapi.miniprojectideas.com/api/TicketsNew/CreateNewTicket';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  createTicket(obj: CreateNewTicket): Observable<any> {
+    return this.http.post(`${this.apiURL}CreateNewTicket`, obj);
+  }
+
+  deleteTicket(id: number) {
+    return this.http.delete(`${this.apiURL}DeleteTicket?id=${id}`);
+  }
+
+}
