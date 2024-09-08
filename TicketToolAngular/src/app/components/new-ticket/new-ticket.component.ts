c192fa53a1c6636f026b599fe35d4f2ac0768601
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2024-09-08 18:53:28 -0500

    add new-ticket component, template and ts

--- TicketToolAngular/src/app/components/new-ticket/new-ticket.component.ts
+++ TicketToolAngular/src/app/components/new-ticket/new-ticket.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { CategoryObj } from 'src/app/interfaces/categoryObj';
+import { CreateNewTicket } from 'src/app/interfaces/create-new-ticket';
 
 @Component({
   selector: 'app-new-ticket',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewTicketComponent implements OnInit {
 
-  constructor() { }
+  formCreateTicket!: FormGroup;
+  parentCategoryList: CategoryObj[] = [];
+  severityList: CreateNewTicket[] = [];
+  selectSeverity: string = '';
+  selectedCategoryName: string = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private toastr: ToastrService,
+  ) {
+    this.formCreateTicket = this.fb.group({
+      employeeId: [0],
+      severityInput: ['', [Validators.required]],
+      childCategoryId: [0],
+      deptId: [0],
+      requestDetails: ['', [Validators.required]],
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  selectSeverityList(severity: CreateNewTicket) {
+    this.formCreateTicket.patchValue({ severityInput: severity.severity});
+    this.selectSeverity = severity.severity;
+  }
+
+  selectCategory(category: CategoryObj) {
+    this.formCreateTicket.patchValue({ parentCategoryId: category.categoryId });
+    this.selectedCategoryName = category.categoryName;
   }
 
 }
